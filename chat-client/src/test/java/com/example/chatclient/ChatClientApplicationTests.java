3df7fcbb7d33b78a7460b3bcbb32fb2d2a88fe1d
MyComputer <ilyanovikov007@mail.ru>
2026-07-20 14:56:14 +0300

    first commit, create application framework

---
+++ chat-client/src/test/java/com/example/chatclient/ChatClientApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.chatclient;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class ChatClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
